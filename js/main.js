2a1b588a90ff792a0cbcc505d786c722ae8dd655
Washington <qtp228@dca9047644ab.cof.ds.capitalone.com>
2017-07-19 10:21:07 -0400

    Added functions to js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@ var cardTwo;
 var cards = ['king', 'king', 'queen', 'queen'];
 var cardsInPlay = [];
 
-cardOne = cards[0];
-cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay[0] === cardsInPlay[1]) {
-  alert('Yay you found a match');
-} else {
-  alert('Sorry try again');
+function checkForMatch() {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert('Yay you found a match');
+  } else {
+    alert('Sorry try again');
+  }
 }
+
+function flipCard(cardID) {
+  console.log('User flipped ' + cards[cardID]);
+
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
