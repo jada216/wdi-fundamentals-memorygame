056ad589cd741498a72d76f979538b04951a86cc
Washington <qtp228@dca9047644ab.cof.ds.capitalone.com>
2017-07-19 10:41:45 -0400

    Added card objects

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-var cardOne;
-var cardTwo;
-var cards = ['king', 'king', 'queen', 'queen'];
+var cards = [
+  {
+    rank: "King",
+    suit: "Hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "King",
+    suit: "Diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
+  {
+    rank: "Queen",
+    suit: "Hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "Queen",
+    suit: "Diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  }
+];
+
 var cardsInPlay = [];
+
+
+
 
 function checkForMatch() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
@@ -12,9 +35,9 @@ function checkForMatch() {
 }
 
 function flipCard(cardID) {
-  console.log('User flipped ' + cards[cardID]);
+  console.log('User flipped ' + cards[cardID].rank);
 
-  cardsInPlay.push(cards[cardID]);
+  cardsInPlay.push(cards[cardID].rank);
   checkForMatch();
 }
 
