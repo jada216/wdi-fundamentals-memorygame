f8a6ce5ca09ec2221ccabc86dbeb48db233c8761
Washington <qtp228@dca9047644ab.cof.ds.capitalone.com>
2017-07-20 13:57:49 -0400

    Multiple rows of cards and updated cards in play list

--- js/main.js
+++ js/main.js
@@ -1,48 +1,46 @@
-var cards = [
-  {
-    rank: "King",
-    suit: "Hearts",
-    cardImage: "images/king-of-hearts.png"
-  },
-  {
-    rank: "King",
-    suit: "Diamonds",
-    cardImage: "images/king-of-diamonds.png"
-  },
-  {
-    rank: "Queen",
-    suit: "Hearts",
-    cardImage: "images/queen-of-hearts.png"
-  },
-  {
-    rank: "Queen",
-    suit: "Diamonds",
-    cardImage: "images/queen-of-diamonds.png"
-  }
-];
+// A constructor for a card object
+function Card(rank, suit, cardImage) {
+  this.rank = rank;
+  this.suit = suit;
+  this.cardImage = cardImage;
+}
 
+// Set the amount of rows you want for the memory game here
+var ROWS = 4;
 var cardsInPlay = [];
 
+var cards = [new Card('King', 'Hearts', 'images/king-of-hearts.png'),
+new Card('King', 'Diamonds', 'images/king-of-diamonds.png'),
+new Card('Queen', 'Hearts', 'images/queen-of-hearts.png'),
+new Card('Queen', 'Diamonds', 'images/queen-of-diamonds.png')];
 
+// Creates the memory game board
 function createBoard() {
-  for(var i = 0; i < cards.length; i++) {
-    var cardElement = document.createElement('img');
-    cardElement.setAttribute('src', 'images/back.png');
-    cardElement.setAttribute('data-id', i);
+  for(var i = 0; i < ROWS; i++) {
+    for (var j = 0; j < cards.length; j++) {
+      var cardElement = document.createElement('img');
+      cardElement.setAttribute('src', 'images/back.png');
+      cardElement.setAttribute('data-id', i);
 
-    cardElement.addEventListener('click', flipCard);
-    document.getElementById('game-board').appendChild(cardElement);
+      cardElement.addEventListener('click', flipCard);
+      document.getElementById('game-board').appendChild(cardElement);
+    }
   }
 }
 
+
+// Called within the 'flipCard' function and checks if two cards match
 function checkForMatch() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
     alert('Yay you found a match');
+    cardsInPlay = [];
   } else {
     alert('Sorry try again');
+    cardsInPlay = [];
   }
 }
 
+// Flips a card and if two cards are flipped, checks for a match
 function flipCard() {
   var cardNum = this.getAttribute('data-id');
   this.setAttribute('src', cards[cardNum].cardImage);
@@ -52,7 +50,7 @@ function flipCard() {
   cardsInPlay.push(cards[cardNum].rank);
 
   if(cardsInPlay.length == 2) {
-    checkForMatch();
+    setTimeout(checkForMatch, 200);
   }
 }
 
